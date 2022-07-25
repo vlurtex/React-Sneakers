bdfaf091ef5cb41bdb6756e1adabcce0af809037
vlurtex <vl.oleynic@gmail.com>
2022-07-25 07:08:50 +0300

    Create_Header_v1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <div className="Wrapper">
+    <div className="wrapper">
       <header>
         <div className="headerLeft">
-          <svg />
+          <img width={40} height={40} src="/img/logo.png" />
           <div className="headerInfo">
             <h3>React Sneakers</h3>
             <p>Магазин лучших кроссовок</p>
@@ -11,15 +11,19 @@ function App() {
         </div>
 
         <ul className="headerRight">
-          <li>
-            <svg />
+          <li className="cart">
+            <img width={18} height={18} src="/img/cart.svg" />
             <span>1205 руб.</span>
           </li>
-          <li>
-            <svg />
+          <li className="user">
+            <img width={18} height={18} src="/img/user.svg" />
           </li>
         </ul>
       </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+        ...
+      </div>
     </div>
   );
 }
