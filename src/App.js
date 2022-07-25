d2b8ec2a1812dec36971c5ddc56bfc72e21233b9
vlurtex <vl.oleynic@gmail.com>
2022-07-25 05:09:57 +0300

    Create react sneakers

---
+++ src/App.js
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div className="Wrapper">
+      <header>
+        <div className="headerLeft">
+          <svg />
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+
+        <ul className="headerRight">
+          <li>
+            <svg />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
